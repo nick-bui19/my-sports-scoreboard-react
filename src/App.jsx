7fea77ac17da028857aa74b47f81711902881ea9
nick bui <tronggiao1908@gmail.com>
2025-04-27 09:16:38 +0200

    fix CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './components/Header';
+import LiveScoreCard from './components/LiveScoreCard';
+import TodayMatches from './components/TodayMatches';
 import React from 'react';
 
 import './App.css'
-
-import Header from './components/Header';
 
 function App() {
   return (
